7c3f698f35d5ddae76a326af03375ea609489ef8
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-01-07 07:59:50 +0530

    Update API endpoint

--- components/dev.ts
+++ components/dev.ts
@@ -1,2 +1,2 @@
-// export const endPoint = "https://foundation-api.onrender.com";
-export const endPoint = "http://localhost:3000";
+export const endPoint = "https://foundation-api.onrender.com";
+// export const endPoint = "http://localhost:3000";
